01478bea26dbb66b321c848b7d9db6f25c5543e4
Xen0ph0n <chris@xenosys.net>
2012-09-10 15:28:17 -0400

    Added commandline options for: Move, Delete, Sleeptime, Yara Rules, and Help

--- midas.py
+++ midas.py
@@ -5,28 +5,45 @@
 # for tracking and association.
 import exiftool
 import os
+import shutil
 import sys
 import pymongo 
 import hashlib
 import datetime
 import time
 import yara
+import argparse
 
+# Database Connection Information
 from pymongo import Connection
 connection = Connection('localhost', 27017)
 db = connection.test
 metadatacollection = db.metadata
-sleeptime = 15
+
+# Define Now to use in Timestamp
 now = datetime.datetime.now()
-rules = yara.compile('./midasyararules.yar')
 
-if len(sys.argv)<2:
-    pathtofiles = os.getcwd()
-else:
-    pathtofiles = sys.argv[1]
+# Argument Parser and Usage Help
+parser = argparse.ArgumentParser(description='Metadata Inspection Database Alerting System')
+parser.add_argument('Path', help='Path to directory of files to be scanned (Required)')
+parser.add_argument('-d','--delete', action='store_true', help='Deletes files after extracting metadata (Default: False)', required=False)
+parser.add_argument('-y','--yararules', default='./midasyararules.yar', help='Specify Yara Rules File (Default: ./midasyararules.yar)', required=False)
+parser.add_argument('-m','--move', help='Where to move files to once scanned (Default: Files are Not Moved)', required=False)
+parser.add_argument('-s','--sleep', type=int, default=15, help='Time in Seconds for Midas.py to sleep between scans (Default: 15 sec)', required=False)
+args = vars(parser.parse_args())
 
+# Set Path to files from Argument
+
+pathtofiles = args['Path']
 print "Path to be scanned: " +  pathtofiles 
 
+# Time to sleep before iterating over target dir again
+sleeptime = args['sleep']
+
+# Location of Yara Rules File
+rules = yara.compile(args['yararules'])
+
+# Md5 Function
 def md5sum(filename):
 	md5 = hashlib.md5()
         with open(filename, 'rb') as f:
@@ -34,6 +51,8 @@ def md5sum(filename):
  			md5.update(chunk)
 	return md5.hexdigest()
 
+
+# Main function which will loop as every X seconds
 def main():
 	while True:
 		for root, dirs, files in os.walk(pathtofiles):
@@ -56,7 +75,13 @@ def main():
 					print  matches 
 				else:
 					print "No Yara Matches "
-			#       os.remove(filename)
+				if args['move']:
+					if not os.path.exists(args['move']):
+						os.makedirs(args['move'])
+					shutil.move(filename, args['move'] + name) 
+				if args['delete'] == True:
+					os.remove(filename)
+					print filename + " has been deleted."
 		time.sleep(sleeptime)
 if __name__ == "__main__":
 	main()  
